4f5b644d80be87c680c532069c797a68bd723ce1
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-13 14:02:47 -0700

    add request factory

--- rayfindr/__init__.py
+++ rayfindr/__init__.py
@@ -1,4 +1,20 @@
 from pyramid.config import Configurator
+from pyramid.request import Request
+from pyramid.request import Response
+
+
+def request_factory(environ):
+    request = Request(environ)
+    if request.is_xhr:
+        request.response = Response()
+        request.response.headerlist = []
+        request.response.headerlist.extend(
+            (
+                ('Access-Control-Allow-Origin', '*'),
+                ('Content-Type', 'application/json')
+            )
+        )
+    return request
 
 
 def main(global_config, **settings):
@@ -9,5 +25,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('json_test', '/json_test')
+    config.set_request_factory(request_factory)
     config.scan()
     return config.make_wsgi_app()
