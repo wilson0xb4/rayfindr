ee1821e7016d96dc4d0ba75859098ec135e09c21
Jim Grant <jimgrant@gmail.com>
2015-07-14 14:41:58 -0700

    first pass functionality

--- rayfindr/calculations/shadows.py
+++ rayfindr/calculations/shadows.py
@@ -15,4 +15,49 @@ def get_shadow_from_points(footprint, sunvector, height):
     Output:
     geoJSON object
     """
-    pass
+
+    # make a list of points representing the projected footprint
+    projection = []
+    vx, vy = sunvector
+    for point in footprint:
+        proj_point = [
+            point[0] + vx * height,
+            point[1] + vy * height
+        ]
+        projection.append(proj_point)
+
+    # for each footprint edge and matching projection edge, make a shadow poly
+    shadowpolys = []
+    for i, point in enumerate(footprint[:-2]):
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint(*point)
+        ring.AddPoint(*projection[i])
+        ring.AddPoint(*projection[i+1])
+        ring.AddPoint(*footprint[i+1])
+        ring.AddPoint(*point)
+
+        poly = ogr.Geometry(ogr.wkbPolygon)
+        poly.AddGeometry(ring)
+        shadowpolys.append(poly)
+        import pdb; pdb.set_trace()
+
+    # union all the shadow polys together
+    unionpoly = shadowpolys[0]
+    for poly in shadowpolys[1:]:
+        unionpoly = unionpoly.Union(poly)
+
+    # export the resulting shape to geoJSON
+    geojson = unionpoly.ExportToJson()
+    return geojson
+
+if __name__ == '__main__':
+    footprint = [
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [0, 0]
+    ]
+    sunvector = [1, 1]
+    height = 2
+    print get_shadow_from_points(footprint, sunvector, height)
