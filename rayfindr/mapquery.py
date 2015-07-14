1b64cb82164d10570f02e6f7680099d584ccfaa0
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-14 15:50:21 -0700

    First pass layer filter

--- rayfindr/mapquery.py
+++ rayfindr/mapquery.py
@@ -2,12 +2,21 @@ from osgeo import ogr
 import shapefile
 
 
-shapefile = "mapdata/seattle.shp"
+shpfile = "mapdata/seattle.shp"
 driver = ogr.GetDriverByName('ESRI Shapefile')
-dataSource = driver.Open(shapefile, 0)
+dataSource = driver.Open(shpfile, 0)
 
 
 def spatial_filter(la_min, la_max, lo_min, lo_max):
     layer = dataSource.GetLayer()
 
+    wkt = (
+        "POLYGON (({la_min} {lo_min},"
+        " {la_max} {lo_max},"
+        " {la_min} {lo_max},"
+        " {la_max} {lo_min}))"
+        .format(la_min=la_min, la_max=la_max, lo_min=lo_min, lo_max=lo_max)
+    )
+    layer.SetSpatialFilter(ogr.CreateGeometryFromWkt(wkt))
 
+    
