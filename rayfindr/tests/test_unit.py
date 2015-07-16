956f97c1c8905abc4649f65e48ee4e8dc00bfcc5
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-16 12:28:21 -0700

    Complete azimuth test

--- rayfindr/tests/test_unit.py
+++ rayfindr/tests/test_unit.py
@@ -7,17 +7,21 @@ def test_receive_location():
     pass
 
 
-def test_sun_location():
-    pass
-
-
 def test_building_data():
     pass
 
+azimuth_input_output = [
+    [datetime(2015, 7, 16, 14), (70, 75)],
+    [datetime(2015, 7, 16, 20), (170, 175)],
+    [datetime(2015, 7, 17, 2), (280, 285)]
+]
 
-def test_get_azimuth():
+
+@pytest.mark.parametrize("date, az_range", azimuth_input_output)
+def test_get_azimuth(date, az_range):
     lat = 47.6062095
     lon = -122.3320708
-    d_time = datetime(2015, 7, 16, 20)
+    d_time = date
     actual = sv.get_azimuth(d_time, lat, lon)
-    assert actual > 140 and actual < 1458
+    az_low, az_high = az_range
+    assert actual > az_low and actual < az_high
