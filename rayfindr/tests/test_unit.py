66e425fc692172c126daaf1d95e5868777889001
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-16 11:46:04 -0700

    Begin to add first pass azimuth test

--- rayfindr/tests/test_unit.py
+++ rayfindr/tests/test_unit.py
@@ -1,4 +1,6 @@
-# import pytest
+import pytest
+import datetime
+import rayfindr.calculations as calc
 
 
 def test_receive_location(app):
@@ -11,3 +13,11 @@ def test_sun_location():
 
 def test_building_data():
     pass
+
+
+def test_get_azimuth():
+    lat = 47.6062095
+    lon = -122.3320708
+    d_time = datetime(2015, 7, 16, 20)
+    actual = calc.get_azimuth(d_time, lat, lon)
+    assert actual > 140 and actual < 1458
