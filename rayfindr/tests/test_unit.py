1f210cc051bae8d986a8a15c4ad5815bcc968090
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-16 14:48:44 -0700

    Update tests

--- rayfindr/tests/test_unit.py
+++ rayfindr/tests/test_unit.py
@@ -23,6 +23,12 @@ altitude_input_output = [
     [datetime(2015, 7, 17, 2), (15, 20)]
 ]
 
+sun_vector_input_output = [
+    [13.1, 72.57, (-1, -1)],
+    [63.52, 171.91, (-1, 1)],
+    [18.07, 282.03, (1, -1)]
+]
+
 
 @pytest.mark.parametrize("date, az_range", azimuth_input_output)
 def test_get_azimuth(date, az_range):
@@ -42,3 +48,17 @@ def test_get_altitude(date, alt_range):
     actual = sv.get_altitude(d_time, lat, lon)
     az_low, az_high = alt_range
     assert actual > az_low and actual < az_high
+
+
+@pytest.mark.parametrize("alt, az, sv_range", sun_vector_input_output)
+def test_get_sun_vector(alt, az, sv_range):
+    vector_x, vector_y = sv.get_sun_vector(alt, az)
+    sign_x, sign_y = sv_range
+    if sign_x < 0:
+        assert vector_x < 0
+    else:
+        assert vector_x >= 0
+    if sign_y < 0:
+        assert vector_y < 0
+    else:
+        assert vector_y >= 0
